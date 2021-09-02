59bfa58f0cfdb6fce0967fa68aabb0dddddbc144
Sanjik761 <sanjarburiniyozov@gmail.com>
2021-09-02 12:05:22 +0500

    corrected js file

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 import { nodeCreator, chainAppend } from './nodeCreator';
+
 const apiKey = 'fb39d41526d21022f068615f1b9f8a6d';
 const search = document.getElementById('search');
 const submit = document.getElementById('submit');
 const container = document.getElementById("container")
 const display = document.querySelector("body")
+
 
 submit.addEventListener('click', (e) => {
   e.preventDefault()
