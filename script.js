0a9912779baa321a8475f696608457a1fd7cecf8
Sanjik761 <sanjarburiniyozov@gmail.com>
2021-09-02 11:23:56 +0500

    creat submit button

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
+const apiKey = 'fb39d41526d21022f068615f1b9f8a6d';
 const search = document.getElementById('search');
 const submit = document.getElementById('submit');
 const container = document.getElementById("container")
 const display = document.querySelector("body")
+
+submit.addEventListener('click', (e) => {
+  e.preventDefault()
+  container.textContent = ""
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${search.value}&appid=${apiKey}`,
+    { mode: 'cors' })
+    .then(function (response) {
+      return response.json();
+    })
+
+
+})
