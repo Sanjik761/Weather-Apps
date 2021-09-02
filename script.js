001a84c2855d0a5c20f849c64f99a79ea646cc68
Sanjik761 <sanjarburiniyozov@gmail.com>
2021-09-02 12:01:48 +0500

    import nodeCreator.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import { nodeCreator, chainAppend } from './nodeCreator';
 const apiKey = 'fb39d41526d21022f068615f1b9f8a6d';
 const search = document.getElementById('search');
 const submit = document.getElementById('submit');
