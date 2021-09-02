b412c76935a92a0bc20e86d76294a67065f5aa6b
Sanjik761 <sanjarburiniyozov@gmail.com>
2021-09-02 11:19:31 +0500

    called the class

---
+++ script.js
@@ -0,0 +1,4 @@
+const search = document.getElementById('search');
+const submit = document.getElementById('submit');
+const container = document.getElementById("container")
+const display = document.querySelector("body")
